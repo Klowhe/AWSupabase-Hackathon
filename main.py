bcbb266af29ca38fd8cef4ebd4336e7ee6688767
Klowhe <e0959208@u.nus.edu>
2024-07-13 18:26:42 +0800

    updated embedding

--- main.py
+++ main.py
@@ -126,6 +126,10 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from openai import OpenAI, RateLimitError
 import backoff
+from langchain_aws import ChatBedrock
+from langchain_core.prompts import ChatPromptTemplate
+from aws_clients.rekognition_client import detect_text
+from aws_clients.transcribe_client import transcribe_audio
 
 # Load environment variables
 load_dotenv()
@@ -141,6 +145,30 @@ rekognition_client = boto3.client('rekognition')
 modelID = "anthropic.claude-v2:1"
 bedrock_client = None  # Initialize later with AWS session
 
+# Create a boto3 session with the new credentials
+session = boto3.Session(
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
+    region_name=os.getenv("AWS_DEFAULT_REGION")
+)
+
+bedrock_client = session.client("bedrock-runtime")
+
+# Initialize ChatBedrock
+llm = ChatBedrock(
+    model_id=modelID,
+    client=bedrock_client,
+    model_kwargs=dict(temperature=0.9)
+)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a chatbot. You are in {language}."),
+        ("human", "{input}")
+    ]
+)
+bedrock_chain = prompt | llm
+
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hey there, I am your friendly legal assistant.")
@@ -149,14 +177,6 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("You have invoked the help command.")
 
 # Helpers
-def detect_text(image_bytes):
-    print("==> Detecting text from image with AWS Rekognition")
-    response = rekognition_client.detect_text(Image={'Bytes': image_bytes})
-    detected_text = []
-    for text_detection in response['TextDetections']:
-        detected_text.append(text_detection['DetectedText'])
-    return ' '.join(detected_text)
-
 @backoff.on_exception(backoff.expo, RateLimitError, max_time=300)
 async def generate_embeddings_openai(text):
     text = text.replace("\n", " ")
@@ -165,30 +185,61 @@ async def generate_embeddings_openai(text):
         return response.data[0].embedding
     return None
 
+def handle_message(text: str):
+    # You can set this based on user preference
+    language = "english"  
+    response = bedrock_chain.invoke({'language': language, 'input': text })
+    response_content = response.content
+    return response_content
+
 # Responses
 async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text: str = update.message.text
-    print(f"==> User Input: {text}")
+    print(f"==> User: {text}")
 
     # Step 1: Generate embeddings for user input
     embeddings = await generate_embeddings_openai(text)
 
     if embeddings:
         print(f"==> Embeddings: {embeddings}")
-        await update.message.reply_text("Embeddings generated successfully.")
+
+        # Step 2: Use embeddings for further processing (querying Bedrock)
+        reply: str = handle_message(text)
+        await update.message.reply_text(reply)
     else:
         await update.message.reply_text("Error generating embeddings. Please try again later.")
 
 async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f"==> User uploaded an image")
+
+    # Download image file for Rekognition
     file = await context.bot.getFile(update.message.photo[-1].file_id)
     image_bytes = await file.download_as_bytearray()
 
     detected_text = detect_text(image_bytes)
-    if detected_text:
-        await update.message.reply_text(f"Detected text:\n{detected_text}")
+    print(f"==> Detected text: {detected_text}")
+    if detected_text == '':
+        await update.message.reply_text("No relevant text is detected from the image. Please try again.")
     else:
-        await update.message.reply_text("No text detected.")
+        reply: str = handle_message(detected_text)
+        await update.message.reply_text(reply)
+
+async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"==> User sent a voice message")
+    
+    # Download voice message for S3
+    file_id = update.message.voice.file_id
+    file = await context.bot.getFile(file_id)
+    file_name = file.file_path.split(f'https://api.telegram.org/file/bot{bot_token}/voice/', 1)[1]
+    await file.download_to_drive(custom_path=f"temp/{file_name}")
+
+    transcript = transcribe_audio(file_name)
+    print(f"==> Transcript: {transcript}")
+    if transcript == '':
+        await update.message.reply_text("Transcription failed. Please try again.")
+    else:
+        reply: str = handle_message(transcript)
+        await update.message.reply_text(reply)
 
 # Errors
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -196,7 +247,7 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == "__main__":
-    print("=== igotscammed has been started ===")
+    print("=== telegram bot has been started ===")
     app = Application.builder().token(bot_token).build()
 
     # Commands
@@ -206,6 +257,7 @@ if __name__ == "__main__":
     # Responses
     app.add_handler(MessageHandler(filters.TEXT, handle_text))
     app.add_handler(MessageHandler(filters.PHOTO, handle_image))
+    app.add_handler(MessageHandler(filters.VOICE, handle_voice))
 
     # Errors
     app.add_error_handler(error)
@@ -213,5 +265,3 @@ if __name__ == "__main__":
     # Polls the bot
     print("==> Polling")
     app.run_polling(poll_interval=3)
-
-
